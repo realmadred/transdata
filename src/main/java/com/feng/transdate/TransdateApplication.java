8a734ba17ab41b567b6e44f506fb4ff82ad4d7c3
realmadred <liufeng2008168@163.com>
2017-09-06 14:37:37 +0800

    修改目录

--- src/main/java/com/feng/transdate/TransdateApplication.java
+++ src/main/java/com/feng/transdate/TransdateApplication.java
@@ -1,4 +1,4 @@
-package com.feng.transdate.transdate;
+package com.feng.transdate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
